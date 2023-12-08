3e8e81efc51eb73ff58e430e41881d0e91720821
IndieDev <tblyduc2412@gmail.com>
2023-12-08 19:27:54 +0700

    Create InitializeDb to Apply migrations on startup.

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,7 @@
 using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using GameStore.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
@@ -9,6 +10,8 @@ var connString = builder.Configuration.GetConnectionString("GameStoreContext");
 builder.Services.AddSqlServer<GameStoreContext>(connString);
 
 var app = builder.Build();
+
+app.Services.InitializeDb();
 
 app.MapGamesEndpoints();
 
